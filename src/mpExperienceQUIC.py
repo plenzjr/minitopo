2870b68a53a64680208d4409da335e08610eb35e
Paulo Lenz <paulogil2010@gmail.com>
2021-10-25 10:17:11 -0300

    test: shutdown interface

--- src/mpExperienceQUIC.py
+++ src/mpExperienceQUIC.py
@@ -106,9 +106,6 @@ class MpExperienceQUIC(MpExperience):
 		self.mpTopo.commandTo(self.mpConfig.client, "ifstat -ntTw &  >> client_ifstat.txt")
 		# SHUTDOWN INTERFACE AFTER 10 SEC
 		self.create_script()
-		self.mpTopo.commandTo(self.mpConfig.client, "ls -l >> infos.txt")
-		self.mpTopo.commandTo(self.mpConfig.client, "pwd >> infos.txt")
-		self.mpTopo.commandTo(self.mpConfig.client, "ifconfig >> infos.txt")
 		self.mpTopo.commandTo(self.mpConfig.client, "sh if_down.sh &")
 
 		cmd = self.getQUICClientPreCmd()
